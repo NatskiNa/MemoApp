94bd1627b76da700e8f5d6154a0149418a2b89a8
Your NameNatski <143272547+NatskiNa@users.noreply.github.com>
2024-03-03 16:59:41 -0800

    markup memolist screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,68 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 const Index = (): JSX.Element => {
   return (
-    <View>
-      <Text>Hello World!</Text>
+    <View style={styles.container}>
+      <View>
+        <View>
+          <Text>Memo App</Text>
+          <Text>Sign out</Text>
+        </View>
+      </View>
+      <View>
+        <View>
+          <View>
+            <Text>shopping list</Text>
+            <Text>date and time</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+
+        <View>
+          <View>
+            <Text>shopping list</Text>
+            <Text>date and time</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+
+        <View>
+          <View>
+            <Text>shopping list</Text>
+            <Text>date and time</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+
+        <View>
+          <View>
+            <Text>shopping list</Text>
+            <Text>date and time</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+      </View>
+      <View>
+        <Text>+</Text>
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+})
 
 export default Index
