c99aad4dd475a0a43b7dbbcb9de977ac06378d6b
Your NameNatski <143272547+NatskiNa@users.noreply.github.com>
2024-03-06 10:28:18 -0800

    logout

--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -1,8 +1,21 @@
-import { TouchableOpacity, Text, StyleSheet } from 'react-native'
+import { TouchableOpacity, Text, StyleSheet, Alert } from 'react-native'
+import { signOut } from 'firebase/auth'
+import { router } from 'expo-router'
+import { auth } from '../config'
+
+const handlePress = (): void => {
+  signOut(auth)
+    .then(() => {
+      router.replace('/auth/login')
+    })
+    .catch(() => {
+      Alert.alert('cannot logout')
+    })
+}
 
 const LogoutButton = (): JSX.Element => {
   return (
-    <TouchableOpacity>
+    <TouchableOpacity onPress={handlePress}>
       <Text style={styles.text}>Logout</Text>
     </TouchableOpacity>
   )
